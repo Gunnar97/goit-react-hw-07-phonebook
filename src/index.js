89356a4a724156eb1410eb59e1223f242ee20b05
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-10-17 13:05:13 +0300

    delete local storage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import './index.css';
 
 import { App } from 'App';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      
         <App />
-      </PersistGate>
+      
       <ToastContainer autoClose={1500} />
     </Provider>
   </>
