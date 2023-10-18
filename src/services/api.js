d078680dcf081145d535cfca068832d65a6a88ee
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-10-18 03:06:06 +0300

    1

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://652d1fb5f9afa8ef4b26d18e.mockapi.io/contacts/',
+  baseURL: 'https://652e5de70b8d8ddac0b13f42.mockapi.io',
 });
 
 export async function fetchContacts() {
-  const response = await API.get();
+  const response = await API.get('/contacts');
   console.log(response.data);
   return response.data;
 }
 
 export async function addContact(newContact) {
-  const response = await API.post('', newContact);
+  const response = await API.post('/contacts/', newContact);
   return response.data;
 }
 
 export async function deleteContact(id) {
-  const response = await API.delete(`${id}`);
+  const response = await API.delete(`/contacts/${id}`);
   return response.data;
 }
